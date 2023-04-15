9c75928c8005551b5fa82a698f61c9406a3d266b
cristopher ontiveros <christoph.alano@gmail.com>
2023-04-14 22:33:33 -0500

    base

---
+++ app/models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(128))
+    lastName = db.Column(db.String(128))
+    email = db.Column(db.String(120), index=True, unique=True)
+    passwordHash = db.Column(db.String(128))
+    registeredDate = db.Column(db.DateTime, default=datetime.utcnow)
+    accountType = db.Column(db.String(64), default='free')
+     
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
+
+    def setPassword(self, password):
+        self.passwordHash = generate_password_hash(password)
+
+    def checkPassword(self, password):
+        return check_password_hash(self.passwordHash, password)
+
+    def getEmail(self):
+        return self.email
+        
+    def getName(self):
+        return f'{self.firstName} {self.lastName}'
+
+    
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
