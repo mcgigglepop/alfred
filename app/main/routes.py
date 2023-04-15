881fdbc5e8fff314d0c05a893180e966cad7cfa4
cristopher ontiveros <christoph.alano@gmail.com>
2023-04-15 01:15:53 -0500

    added income accounts page

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,3 +7,10 @@ def expenses():
     Route and method for rendering the expenses page.
     """
     return render_template('internal/expenses.html', title='Expenses')
+
+@bp.route('/income', methods=['GET', 'POST'])
+def income():
+    """
+    Route and method for rendering the income accounts page.
+    """
+    return render_template('internal/income.html', title='Income Accounts')
