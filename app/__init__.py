16decaafe940fce7b13754f2d02773994f222b62
cristopher ontiveros <christoph.alano@gmail.com>
2023-04-17 23:25:18 -0500

    added the app init for auth routes

--- app/__init__.py
+++ app/__init__.py
@@ -26,6 +26,9 @@ def create_app(config_class=Config):
     csrf = CSRFProtect()
     csrf.init_app(app)
 
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
